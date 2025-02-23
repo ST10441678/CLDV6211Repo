f363fa920129edd61cd3df668a09a71ca2b685d4
ST10441678 <ST10441678@vcconnect.edu.za>
2025-02-23 20:58:57 +0200

    Add project files.

---
+++ MyvcMovie/Data/MyvcMovieContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace MyvcMovie.Data
+{
+    public class MyvcMovieContext : DbContext
+    {
+        public MyvcMovieContext (DbContextOptions<MyvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Models.Movie> Movie { get; set; } = default!;
+    }
+}
